d1c5e8b4e73c69ff640730f6657cd93b3eda6a47
JiwonKim08 <99666136+JiwonKim08@users.noreply.github.com>
2022-05-17 12:36:37 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import TodoData from "./data.json";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const [todoEditing, setTodoEditing] = useState(null);
+  const [editingText, setEditingText] = useState("");
+
+  useEffect(() => {
+    const loadedTodos = TodoData;
+    setTodos(loadedTodos);
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+    setTodos([...todos].concat(newTodo));
+    setTodo("");
+  }
+
+  function deleteTodo(id) {
+    let updatedTodos = [...todos].filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
+  return (
+    <div id="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => (
+        <div key={todo.id} className="todo">
+          <div className="todo-text">
+            <input
+              type="checkbox"
+              id="completed"
+              checked={todo.completed}
+              onClick={!todo.completed}
+            />
+
+            {todo.id === todoEditing ? (
+              <input
+                type="text"
+                value={editingText}
+                onChange={(e) => setEditingText(e.target.value)}
+              />
+            ) : (
+              <div>{todo.text}</div>
+            )}
+          </div>
+          <div className="todo-actions">
+            <div className="todo-edit">
+              <button onClick={setTodoEditing(todo.id)}>Edit</button>
+              <button type="submit">Submit Edits</button>
+            </div>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
